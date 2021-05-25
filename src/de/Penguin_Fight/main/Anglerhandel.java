d148a123988793bbd6e606f4f666e7961985eb84
penguinfight <79137752+penguinfight@users.noreply.github.com>
2021-05-25 17:38:09 +0200

    Update Anglerhandel.java

--- src/de/Penguin_Fight/main/Anglerhandel.java
+++ src/de/Penguin_Fight/main/Anglerhandel.java
@@ -27,7 +27,7 @@ public class Anglerhandel {
 
 	
 	public static void main(String[] args) throws LoginException {		
-		String token = "ODA0MzUyNDM5MDE2MjI2ODE3.YBLFlQ.5MvXYoIozrqPEv1w5STDLBI0t78";
+		String token = System.getEnv("TOKEN");
 								
 		builder = JDABuilder.createDefault(token);
 		
